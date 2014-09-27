a83e8e87ace43f8387714c8644407f2a842f97fb
wfudong <wfudong0306@gmail.com>
2014-09-27 18:54:30 +0800

    test aa

---
+++ hello/src/hello/Test_hello.java
@@ -0,0 +1,11 @@
+package hello;
+
+public class Test_hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("what hell are you doing?");
+
+	}
+
+}
